7273ad4f8f005e1663d02349387e0fa897550eef
Scott Winkler <scottjwinkler@gmail.com>
2017-04-26 02:14:39 -0700

    test deploy 1.3

--- server/index.js
+++ server/index.js
@@ -1,18 +1,45 @@
 const express = require('express');
 const path = require('path');
-
+const bodyParser=require('body-parser');
+var MongoClient = require('mongodb').MongoClient
+const assert=require('assert');
 const app = express();
 const PORT = process.env.PORT || 5000;
-
+var url = 'mongodb://root:1234@ds119151.mlab.com:19151/heroku_176r45lm';
 // Priority serve any static files.
 app.use(express.static(path.resolve(__dirname, '../react-ui/build')));
 
 // Answer API requests.
-app.get('/api', function (req, res) {
-  res.set('Content-Type', 'application/json');
-  res.send('{"message":"Hello from the custom server!"}');
-});
+app.push('/db',function(req,res){
+  id_user = req.body.id_user;
+  var searched = req.body.searched;
+  var cur_date=new Date();
+  MongoClient.connect(url, function (err, db) {
+    assert.equal(null, err);
+    // Insert a single document
+    var searches = db.collection('searches');
+    searches.insertOne({
+      id_user,
+      searched,
+      date: cur_date
+    }, {
+        w: 'majority'
+        , wtimeout: 10000
+        , serializeFunctions: true
+      }, function (err, r) {
+        assert.equal(null, err);
+        assert.equal(1, r.insertedCount);
+      });
 
+    if (searches.find(
+      {
+       // $and: [
+         // { "searched": searched }, { "id_user": { $ne: id_user }, }
+        //]
+      }
+    )) { res.send("FOUND") }
+    db.close();
+})})
 // All remaining requests return the React app, so it can handle routing.
 app.get('*', function(request, response) {
   response.sendFile(path.resolve(__dirname, '../react-ui/build', 'index.html'));
