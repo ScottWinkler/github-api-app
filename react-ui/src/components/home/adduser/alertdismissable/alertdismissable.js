16560aec2b51d74c59e3346e91f02b7146825724
Scott Winkler <scottjwinkler@gmail.com>
2017-04-26 15:07:23 -0700

    hopefully final

--- react-ui/src/components/home/adduser/alertdismissable/alertdismissable.js
+++ react-ui/src/components/home/adduser/alertdismissable/alertdismissable.js
@@ -10,7 +10,7 @@ export default class AlertDismissable extends Component {
             <Alert bsStyle="danger" onDismiss={this.props.handleAlertDismiss}>
            {this.props.error==="GET_USER_ERROR" ? (<div><h4>GitHub User Not Found!</h4>
           <p>Please check your spelling and try again</p> </div>) :null}     
-          {this.props.error==="NOT_UNIQUE_USER" ? (<div><h4>Not a Unique Search!</h4>
+          {this.props.error==="NOT_UNIQUE_SEARCH" ? (<div><h4>Not a Unique Search!</h4>
           <p>Someone else searched for this within the last two minutes</p> </div>) :null}   
             <Button bsStyle="danger" onClick={this.props.handleAlertDismiss}> Close</Button>
             </Alert>
